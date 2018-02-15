8d3b8c149a983e52a790026705628d60f6e6c6d9
milagrospalma <mnpalma.2@gmail.com>
2018-02-14 21:25:03 -0500

    obtiene ubicación actual

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,18 @@
-function initMap() {
-  var point = {
-    lat: -11.976176,
-    lng: -77.083082
-  };
+window.addEventListener('load', function() {
+  findLocation();
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: point
-  });
-
-  var marker = new google.maps.Marker({
-    position: point,
-    map: map
-  });
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      marker.setPosition(pos);
-      map.setCenter(pos);
-      console.log(pos);
-    }, function() {
-      handleLocationError(true, marker, map.getCenter());
-    });
-  } else {
-    handleLocationError(false, marker, map.getCenter());
+  function findLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var location = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        console.log(location.lat);
+        console.log(location.lng);
+      });
+    } else {
+      console.log('error');
+    }
   }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-    'Error: Your browser doesn\'t support geolocation.');
-};
+});
