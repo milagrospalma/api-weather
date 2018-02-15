44456fcf1f8766c8324928f5b1930637d3ce7515
milagrospalma <mnpalma.2@gmail.com>
2018-02-15 00:26:44 -0500

    trabajando datos

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,12 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
+  var icon = document.getElementById('icon');
+  var temperature = document.getElementById('temperature');
+  var wind = document.getElementById('wind');
+  var humidity = document.getElementById('humidity');
+  var uv = document.getElementById('uv');
+  var pressure = document.getElementById('pressure');
+  var btnWeek = document.getElementById('week');
+
   findLocation();
 
   function findLocation() {
@@ -10,9 +18,27 @@ window.addEventListener('load', function() {
         };
         console.log(location.lat);
         console.log(location.lng);
+        var proxy = 'https://cors-anywhere.herokuapp.com/';
+        var apiDarkSky = `https://api.darksky.net/forecast/830e41c8feba04c962c6384dfe3958ec/${location.lat},${location.lng}`;
+        // console.log(apiDarkSky);
+
+        //   $.ajax({
+        //     url: proxy + apiLinkDS,
+        //     success: function(data) { // -> llamar a la función showWeather
+        //       console.log('hola'); 
+        //     }
+        //   });
       });
     } else {
       console.log('error');
     }
   }
+
+  function showWeather(data) {
+    // trabajar la data
+  }
+
+  btnWeek.addEventListener('click', function() {
+    window.location.href = 'views/week.html';
+  });
 });
